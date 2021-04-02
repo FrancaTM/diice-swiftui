3894a125639153ccfeb621c36577c1160d75391f
FrancaTM <francatm@outlook.com>
2021-04-02 14:37:17 -0300

    implement UI

--- diice-swiftui/ContentView.swift
+++ diice-swiftui/ContentView.swift
@@ -9,7 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                HStack {
+                    Image("logo")
+                        .resizable()
+                        .frame(width: 64, height: 64)
+                    Text("Diice")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                }
+                Spacer()
+                HStack {
+                    DiceView(number: 1)
+                    DiceView(number: 6)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {}) {
+                    Text("Roll")
+                        .font(.system(size: 55))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.orange)
+            }
+        }
+    }
+}
+
+struct DiceView: View {
+    let number: Int
+    
+    var body: some View {
+        Image("dice\(number)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
             .padding()
     }
 }
@@ -19,3 +59,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
